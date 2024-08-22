455c96a823fe3595324ed4e674bb3bd316f36a75
khanghyper <khangnd1806@gmail.com>
2024-08-22 21:23:03 +0700

    code a path of header

---
+++ src/app/(guest)/_components/guest-header.tsx
@@ -0,0 +1,14 @@
+
+export default function GuestHeader() {
+  return (
+    <div className="w-full flex justify-center">
+      <div className="w-content border h-[130px]">
+        <div className="top-nav w-full flex gap-4 py-2 text-[14px] text-[#8E8181] font-semibold">
+          <span>Tải ứng dụng</span>
+          <span>Chăm sóc khách hàng</span>
+          <span>Nhà cung cấp</span>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/app/(shop)/shop/layout.tsx
@@ -0,0 +1,9 @@
+export default function ShopLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      header Shop
+      {children}
+      footer Shop
+    </div>
+  )
+}
