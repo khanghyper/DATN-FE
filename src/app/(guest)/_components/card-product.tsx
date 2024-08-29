980791d4fbff80eae8621094dad860e49d3a415c
khanghyper <khangnd1806@gmail.com>
2024-08-29 13:51:14 +0700

    add logo

--- src/app/(guest)/_components/card-product.tsx
+++ src/app/(guest)/_components/card-product.tsx
@@ -1,10 +1,13 @@
 import { Star } from "lucide-react";
+import Link from "next/link";
 
 export default function CardProduct() {
   return (
     <div className="card-product py-1 shadow rounded border">
       <div className="img w-full px-5 pt-2 h-[200px]">
-        <img className="size-full object-cover" src="https://salt.tikicdn.com/cache/750x750/ts/product/59/6d/d2/3a4ee4a2ffbae124699e5a01ab5cf5db.jpg.webp" alt="" />
+        <Link href={'/products/1'}>
+          <img className="size-full object-cover" src="https://salt.tikicdn.com/cache/750x750/ts/product/59/6d/d2/3a4ee4a2ffbae124699e5a01ab5cf5db.jpg.webp" alt="" />
+        </Link>
       </div>
       <div className="p-2 flex flex-col gap-0.5">
         <span className="text-[16px] font-medium">Điện thoại SAMSUNG</span>
