78fb92a2865bea3cb33dc378fd65b425419d8b1b
minhtuan11102003 <hain77631@gmail.com>
2024-09-13 03:25:35 +0700

    update page

--- src/app/(guest)/categories/page.tsx
+++ src/app/(guest)/categories/page.tsx
@@ -3,7 +3,66 @@ import React from 'react';
 import CardProduct from '../_components/card-product';
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb';
 import CategoriesIconGuest from '../_components/categoriesIcon';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { title } from 'process';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
 
+const categoriesList: any = [
+  {
+    title: 'Dụng cụ nhà bếp',
+    categoryChildren: [
+      { title: 'Ấm nước các loại' },
+      { title: 'Dụng cụ chứa đựng thực phẩm' },
+      { title: 'Dao, kéo và phụ kiện' },
+      { title: 'Dụng cụ làm bánh' },
+      { title: 'Kệ nhà bếp' },
+      { title: 'Bếp nướng, vỉ nướng' },
+      { title: 'Phụ kiện nhà bếp' },
+      { title: 'Nồi và chảo' },
+      { title: 'Bộ hộp cơm và phụ kiện' },
+      { title: 'Thùng đựng gạo' },
+    ],
+  }, {
+    title: 'Đồ dùng nấu ăn'
+  }, {
+    title: 'Nội thất'
+  }
+];
+
+const filterCate: any = [
+  {
+    title: 'Thương hiệu',
+    opt: [
+      { title: 'Tundo' },
+      { title: 'DAICAT' },
+      { title: 'Total' },
+      { title: 'Makita' },
+      { title: 'Khác' },
+    ],
+  },
+  {
+    title: 'Phân loại',
+    opt: [
+      { title: 'Bộ chăn ga gối' },
+      { title: 'Bộ ga gối' },
+      { title: 'Gối' },
+      { title: 'Khác' },
+    ],
+  },
+];
 
 
 const CategoriesPage = () => {
@@ -23,87 +82,69 @@ const CategoriesPage = () => {
       <div className='bottom-cate w-content flex py-4 justify-between'>
         <div className="left-bottom-cate w-[208px] h-auto ">
           <div className="list-cate flex flex-col w-full ">
-            <h2 className='font-bold'>Khám phá theo danh mục</h2>
+            <h2 className='font-bold text-[17px] mt-2'>Khám phá theo danh mục</h2>
             <div className="section1-list-cate w-full flex flex-col gap-1 pl-[4px] mt-2">
-              <div className="title-section flex justify-between pr-2">
-                <h3 className='font-semibold'>Dụng cụ nhà bếp</h3>
-                <ChevronDown className='' />
-              </div>
-              <div className='content-section flex flex-col w-full gap-2 mt-1'>
-                <span className='cursor-pointer'>Ấm nước các loại</span>
-                <span className='cursor-pointer'>Dụng cụ chứa đựng thực phẩm</span>
-                <span className='cursor-pointer'>Dao, kéo và phụ kiện</span >
-                <span className='cursor-pointer'>Dụng cụ làm bánh</span >
-                <span className='cursor-pointer'>Kệ nhà bếp</span >
-                <span className='cursor-pointer'>Bếp nướng, vỉ nướng</span >
-                <span className='cursor-pointer'>Phụ kiện nhà bếp</span >
-                <span className='cursor-pointer'>Nồi và chảo</span >
-                <span className='cursor-pointer'>Bộ hộp cơm và phụ kiện</span >
-                <span className='cursor-pointer'>Thùng đựng gạo</span >
-              </div>
-            </div>
-            <div className="section2-list-cate w-full flex flex-col gap-1 pl-[4px] mt-2">
-              <div className="title-section flex justify-between pr-2">
-                <h3 className='font-semibold'>Đồ dùng phòng ăn</h3>
-                <ChevronDown className='' />
-              </div>
-            </div>
-            <div className="section3-list-cate w-full flex flex-col gap-1 pl-[4px] mt-2">
-              <div className="title-section flex justify-between pr-2 ">
-                <h3 className='font-semibold'>Trang bị nhà cửa</h3>
-                <ChevronDown className='' />
-              </div>
+              <Accordion type="single" collapsible>
+                {
+                  categoriesList.map((it: any, index: number) => (
+                    <AccordionItem value={`item-${index}`} key={index} >
+                      <AccordionTrigger className='font-semibold text-[16px] pb-2'>{it.title}</AccordionTrigger>
+                      {
+                        it.categoryChildren && it.categoryChildren.map((ct: any, childIndex: number) => (
+                          <AccordionContent className='pl-1 text-[14px] font-medium pb-2' key={childIndex}>
+                            {ct.title}
+                          </AccordionContent>
+                        ))
+                      }
+                    </AccordionItem>
+                  ))
+                }
+              </Accordion>
             </div>
           </div>
+          <div className="list-cate flex flex-col w-full ">
+            <h2 className='font-bold text-[17px] mt-2'>Lọc sản phẩm</h2>
+            <div className="section1-list-cate w-full flex flex-col gap-1 pl-[4px] mt-2">
+              <Accordion type="single" collapsible>
+                {
+                  filterCate.map((it: any, index: number) => (
+                    <AccordionItem value={`item-${index}`} key={index}>
+                      <AccordionTrigger className='font-semibold text-[16px] pb-2'>{it.title}</AccordionTrigger>
+                      {
+                        it.opt && it.opt.map((cd: any, index: number) => (
+                          <AccordionContent key={index} className='pl-1 text-[14px] font-medium pb-2'>
+                            {cd.title}
+                          </AccordionContent>
+                        ))
+                      }
+                    </AccordionItem>
+                  ))
+                }
+              </Accordion>
+            </div>
+          </div>
+
         </div>
         <div className="right-bottom-cate w-[971px] h-auto">
-          <div className="title-right-cate w-full h-[74px] flex items-center">
+          {/* <div className="title-right-cate w-full h-[74px] flex items-center">
             <h1 className='font-extrabold text-[20px] pl-[14px]'>Nhà cửa & Đời sống</h1>
-          </div>
+          </div> */}
           <div className="banner-right-cate w-full h-[190px] flex gap-3">
             <img src="./images/banner1.webp" className='w-1/2 rounded ' />
             <img src="./images/banner1.webp" className='w-1/2 rounded' />
           </div>
-          <CategoriesIconGuest/>
-          <div className="option-cate-right w-full h-[153px] pt-4 ">
-            <div className="title w-full h-[24px] ">
-              <h2 className='font-bold text-[18px]'>Tất cả sản phẩm</h2>
-            </div>
-            <div className="opt-mid w-full h-[82px] flex justify-between items-center border-b-2">
-              <div className="left-opt w-[774px] h-[57px] flex gap-5">
-                <div className="thuongHieu w-[350px] flex flex-col">
-                  <div className="title w-full">
-                    <p className='text-gray-400 text-[12px]'>Thương hiệu</p>
-                  </div>
-                  <div className="opt flex w-full gap-2 items-center">
-                    <span className='text-[14px] border px-3 py-1 rounded-[15px] cursor-pointer'>Tundo</span>
-                    <span className='text-[14px] border px-3 py-1 rounded-[15px] cursor-pointer'>DAICAT</span>
-                    <span className='text-[14px] border px-3 py-1 rounded-[15px] cursor-pointer'>Total</span>
-                    <span className='text-[14px] border px-3 py-1 rounded-[15px] cursor-pointer'>Makita</span>
-                    <span className='text-[14px] border px-3 py-1 rounded-[15px] cursor-pointer'><ChevronDown /></span>
-                    <span className=' text-[24px] font-light text-gray-400'>|</span>
-                  </div>
-                </div>
-                <div className="phanLoai w-[424px] flex flex-col">
-                  <div className="title w-full">
-                    <p className='text-gray-400 text-[12px]'>Phân loại</p>
-                  </div>
-                  <div className="opt flex w-full gap-2 items-center ">
-                    <span className='text-[14px] border px-3 py-1 rounded-[15px] cursor-pointer'>Bộ chăn ga</span>
-                    <span className='text-[14px] border px-3 py-1 rounded-[15px] cursor-pointer'>Bộ ga gối</span>
-                    <span className='text-[14px] border px-3 py-1 rounded-[15px] cursor-pointer'>Gối</span>
-                    <span className='text-[14px] border px-3 py-1 rounded-[15px] cursor-pointer'>Khác</span>
-                    <span className='text-[14px] border px-3 py-1 rounded-[15px] cursor-pointer'><ChevronDown /></span>
-                    <span className='text-[24px] font-light text-gray-400'>|</span>
-                  </div>
-                </div>
+          <CategoriesIconGuest />
+          <div className="option-cate-right w-full h-[103px] pt-4 ">
+            <div className='w-full flex items-center'>
+              <div className="title w-full h-[24px] ">
+                <h2 className='font-bold text-[18px]'>Tất cả sản phẩm</h2>
               </div>
               <div className="mt-2 right-opt w-[95px] h-[36px] flex gap-3 border rounded-[15px] px-2 py-1 items-center cursor-pointer">
                 <Filter size={18} />
                 <h2 className='text-[14px]'>Tất cả</h2>
               </div>
             </div>
-            <div className="opt-bottom w-full h-[24px] flex justify-between items-center mt-1 text-[14px]">
+            <div className="opt-bottom w-full h-[24px] flex justify-between items-center mt-4 text-[14px]">
               <div className="left-bottom w-600px h-full flex items-center gap-3">
                 <div className='flex gap-1 items-center'>
                   <span className='border py-3 px-3 rounded-[10px] bg-gray-200'></span>
@@ -144,6 +185,22 @@ const CategoriesPage = () => {
               }
             </div>
           </div>
+          <Pagination>
+            <PaginationContent>
+              <PaginationItem>
+                <PaginationPrevious href="#" />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink href="#">1</PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink href="#">2</PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationNext href="#" />
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
         </div>
       </div>
     </>
--- src/app/(guest)/sale/page.tsx
+++ src/app/(guest)/sale/page.tsx
@@ -4,10 +4,23 @@ import { Button } from '@/components/ui/button';
 import CategoriesIconGuest from '../_components/categoriesIcon';
 import { array } from 'zod';
 import CardProduct from '../_components/card-product';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb';
+import VoucherComponent from '../_components/voucher';
 
 const SieuSalePage = () => {
   return (
     <div className='w-content h-auto '>
+      <Breadcrumb className="pb-5">
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Trang chủ</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage className='font-semibold'>Sale</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
       <div className="banner-sale w-[1179px] h-[247px] flex gap-3 mt-3">
         <div className="banner-left w-1/2  h-[247px] ">
           <img src="./images/banner1.webp" className="rounded-[5px] w-[587px] h-[247px]" />
@@ -16,102 +29,14 @@ const SieuSalePage = () => {
           <img src="./images/banner2.webp" className="rounded-[5px] w-[587px] h-[247px]" />
         </div>
       </div>
-      <div className="section2 w-full h-[154px] mt-6">
+      <div className="section2 w-full h-auto mt-6">
         <h1 className='font-bold text-[28px] w-full h-[24px] text-[#FF0000] flex justify-center items-center pt-2'>ĐẠI HỘI SIÊU SALE</h1>
-        <div className="sale-voucher-section flex w-full h-[92px] justify-center items-center mt-5 gap-3">
-          <div className='w-[318px] h-[92px] flex'>
-            <div className='w-[212px] h-full shadow border-y-2 border-l-2'>
-              <div className='top w-full h-[60px] flex justify-between  px-1 rounded-l-[5px]'>
-                <div className='w-[157px] flex flex-col font-bold text-[15px] justify-center text-[#D5600C]'>
-                  <span>Giảm giá 200K đơn từ</span>
-                  <span>2Tr</span>
-                </div>
-                <div className='w-[48px] h-[38px] text-[12px] flex items-center justify-end'>
-                  <span className='text-[#0F62FE]'>Chi tiết</span>
-                </div>
-              </div>
-              <div className='bottom w-full h-[32px] flex gap-2 px-1 items-center '>
-                <span className='text-[11px] font-semibold text-gray-400'>HSD</span>
-                <span className='text-[12px] font-bold text-[#3F4B53]'>16:59 31/08/2024</span>
-              </div>
-            </div>
-            <div className='w-[16px] h-full flex flex-col'>
-              <div className='border-b-4  w-[16px] h-[10px] rounded-b-[44px]'>
-
-              </div>
-              <div className=' w-[16px] h-[72px]  rounded-t-[-24px]'>
-
-              </div>
-              <div className='border-t-4 w-[16px] h-[10px] rounded-t-[44px]'>
-
-              </div>
-            </div>
-            <div className='w-[90px] h-full flex items-center justify-center rounded-r-[5px] shadow border-y-2 border-r-2'>
-              <Button className='bg-blue-600'>Lưu</Button>
-            </div>
-          </div>
-          <div className='w-[318px] h-[92px] flex'>
-            <div className='w-[212px] h-full shadow border-y-2 border-l-2'>
-              <div className='top w-full h-[60px] flex justify-between  px-1 rounded-l-[5px]'>
-                <div className='w-[157px] flex flex-col font-bold text-[15px] justify-center text-[#D5600C]'>
-                  <span>Giảm giá 200K đơn từ</span>
-                  <span>2Tr</span>
-                </div>
-                <div className='w-[48px] h-[38px] text-[12px] flex items-center justify-end'>
-                  <span className='text-[#0F62FE]'>Chi tiết</span>
-                </div>
-              </div>
-              <div className='bottom w-full h-[32px] flex gap-2 px-1 items-center '>
-                <span className='text-[11px] font-semibold text-gray-400'>HSD</span>
-                <span className='text-[12px] font-bold text-[#3F4B53]'>16:59 31/08/2024</span>
-              </div>
-            </div>
-            <div className='w-[16px] h-full flex flex-col'>
-              <div className='border-b-4  w-[16px] h-[10px] rounded-b-[44px]'>
-
-              </div>
-              <div className=' w-[16px] h-[72px]  rounded-t-[-24px]'>
-
-              </div>
-              <div className='border-t-4 w-[16px] h-[10px] rounded-t-[44px]'>
-
-              </div>
-            </div>
-            <div className='w-[90px] h-full flex items-center justify-center rounded-r-[5px] shadow border-y-2 border-r-2'>
-              <Button className='bg-blue-600'>Lưu</Button>
-            </div>
-          </div>
-          <div className='w-[318px] h-[92px] flex'>
-            <div className='w-[212px] h-full shadow border-y-2 border-l-2'>
-              <div className='top w-full h-[60px] flex justify-between  px-1 rounded-l-[5px]'>
-                <div className='w-[157px] flex flex-col font-bold text-[15px] justify-center text-[#D5600C]'>
-                  <span>Giảm giá 200K đơn từ</span>
-                  <span>2Tr</span>
-                </div>
-                <div className='w-[48px] h-[38px] text-[12px] flex items-center justify-end'>
-                  <span className='text-[#0F62FE]'>Chi tiết</span>
-                </div>
-              </div>
-              <div className='bottom w-full h-[32px] flex gap-2 px-1 items-center '>
-                <span className='text-[11px] font-semibold text-gray-400'>HSD</span>
-                <span className='text-[12px] font-bold text-[#3F4B53]'>16:59 31/08/2024</span>
-              </div>
-            </div>
-            <div className='w-[16px] h-full flex flex-col'>
-              <div className='border-b-4  w-[16px] h-[10px] rounded-b-[44px]'>
-
-              </div>
-              <div className=' w-[16px] h-[72px]  rounded-t-[-24px]'>
-
-              </div>
-              <div className='border-t-4 w-[16px] h-[10px] rounded-t-[44px]'>
-
-              </div>
-            </div>
-            <div className='w-[90px] h-full flex items-center justify-center rounded-r-[5px] shadow border-y-2 border-r-2'>
-              <Button className='bg-blue-600'>Lưu</Button>
-            </div>
-          </div>
+        <div className="sale-voucher-section grid grid-cols-3 w-full justify-center items-center mt-5 gap-3">
+          {
+            Array.from({ length: 6 }, (_, i) => i + 1).map(item => (
+              <VoucherComponent key={item} />
+            ))
+          }
         </div>
       </div>
       <CategoriesIconGuest />
