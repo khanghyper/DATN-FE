721442c6389d2d3c6ee0d7781a300f536b4c8ebf
minhtuan11102003 <hain77631@gmail.com>
2024-10-24 18:39:10 +0700

    update nhẹ

--- src/app/(guest)/_components/card-product.tsx
+++ src/app/(guest)/_components/card-product.tsx
@@ -1,30 +1,48 @@
+'use client'
 import { Star } from "lucide-react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { any } from "zod";
 
-export default function CardProduct({ name, image }: { name: string, image: string }) {
+export default function CardProduct(props: any,limit:number) {
+  const [products, setProducts] = useState<any>([]);
+  useEffect(() => {
+    if (Array.isArray(props.data)) {
+      const randomData = [...props.data].sort(()=> Math.random() - 0.5)
+      randomData.slice(0,5);
+      setProducts(randomData); 
+    }
+  }, [props.data]); 
 
+  
   return (
-    <div className="card-product py-1 shadow rounded border">
-      <div className="img w-full px-5 pt-2 h-[200px]">
-        <Link href={'/products/1'}>
-          <img className="size-full object-cover" src={image ? image : 'https://images.unsplash.com/photo-1517037673086-5d09c5e1a537?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'} alt="" />
-        </Link>
-      </div>
-      <div className="p-2 flex flex-col gap-0.5">
-        <span className="text-[16px] font-medium">{name}</span>
-        <span className="text-[18px] font-semibold text-red-600">345.000đ</span>
-        <div className="inline">
-          <span className="text-[13px] bg-gray-100 rounded-sm px-2">Trả góp Muadee</span>
-        </div>
-        <div className="text-[12px]">Đã bán 128</div>
-        <div className="flex justify-between items-center text-[12px]">
-          <div className="flex gap-1 items-center">
-            <div className="leading-4">5/5</div>
-            <Star size={12} className="text-yellow-500" />
+    <>
+      {
+        products.slice(0,5).map((item:any, index:number) => (
+          <div className="card-product py-1 shadow rounded border" key={index}>
+            <div className="img w-full px-5 pt-2 h-[200px]">
+              <Link href={`/products/${item.id}`}>
+                <img className="size-full object-cover" src="https://salt.tikicdn.com/cache/750x750/ts/product/59/6d/d2/3a4ee4a2ffbae124699e5a01ab5cf5db.jpg.webp" alt="" />
+              </Link>
+            </div>
+            <div className="p-2 flex flex-col gap-0.5">
+              <span className="text-[16px] font-medium">{item.title}</span>
+              <span className="text-[18px] font-semibold text-red-600">{item.price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}đ</span>
+              <div className="inline">
+                <span className="text-[13px] bg-gray-100 rounded-sm px-2">Trả góp Muadee</span>
+              </div>
+              <div className="text-[12px]">Đã bán 128</div>
+              <div className="flex justify-between items-center text-[12px]">
+                <div className="flex gap-1 items-center">
+                  <div className="leading-4">5/5</div>
+                  <Star size={12} className="text-yellow-500" />
+                </div>
+                <span>Đồng Nai</span>
+              </div>
+            </div>
           </div>
-          <span>Đồng Nai</span>
-        </div>
-      </div>
-    </div>
+        ))
+      }
+    </>
   )
 }
--- src/app/(guest)/_components/hang-xin-section.tsx
+++ src/app/(guest)/_components/hang-xin-section.tsx
@@ -1,42 +1,42 @@
-import productApiRequest from "@/apiRequest/product";
+'use client'
 import CardProduct from "@/app/(guest)/_components/card-product";
-import { useEffect, useState } from "react";
 
+const arrayProducts: { id: number, title: string, price: number }[] = [
+  { id: 1, title: "Sản phẩm 1", price: 150000 },
+  { id: 2, title: "Sản phẩm 2", price: 160000 },
+  { id: 3, title: "Sản phẩm 3", price: 170000 },
+  { id: 4, title: "Sản phẩm 4", price: 180000 },
+  { id: 5, title: "Sản phẩm 5", price: 190000 },
+  { id: 6, title: "Sản phẩm 6", price: 200000 }
+]
 
-export default async function HangXinSection() {
-  try {
-    const data = await productApiRequest.findAll();
+export default function HangXinSection() {
 
-    return (
-      <div className="w-full">
-        <div className="w-full py-2 flex justify-between items-center">
-          <div className="flex gap-2 items-center">
-            <span className="text-[18px] font-bold">Hàng xịn giá sốc</span>
-            <div className="flex gap-1 items-center">
-              <div className="bg-red-500 text-[14px] rounded w-[26px] h-6 flex items-center justify-center text-white">07</div>
-              <span className="text-[20px] font-bold">:</span>
-              <div className="bg-red-500 text-[14px] rounded w-[26px] h-6 flex items-center justify-center text-white">24</div>
-              <span className="text-[20px] font-bold">:</span>
-              <div className="bg-red-500 text-[14px] rounded w-[26px] h-6 flex items-center justify-center text-white">29</div>
-            </div>
+  return (
+    <div className="w-full">
+      <div className="w-full py-2 flex justify-between items-center">
+        <div className="flex gap-2 items-center">
+          <span className="text-[18px] font-bold">Hàng xịn giá sốc</span>
+          <div className="flex gap-1 items-center">
+            <div className="bg-red-500 text-[14px] rounded w-[26px] h-6 flex items-center justify-center text-white">07</div>
+            <span className="text-[20px] font-bold">:</span>
+            <div className="bg-red-500 text-[14px] rounded w-[26px] h-6 flex items-center justify-center text-white">24</div>
+            <span className="text-[20px] font-bold">:</span>
+            <div className="bg-red-500 text-[14px] rounded w-[26px] h-6 flex items-center justify-center text-white">29</div>
           </div>
-          <div className="text-[13px] text-blue-500 cursor-pointer underline font-semibold">Xem tất cả</div>
         </div>
-        <div className="list-card-product py-3 grid grid-cols-5 gap-4">
-          {data.payload.data.data.map((item: any, index: number) => (
-            <CardProduct key={index} name={item.name} image={item.image} />
-          ))}
-        </div>
+        <div className="text-[13px] text-blue-500 cursor-pointer underline font-semibold">Xem tất cả</div>
       </div>
-    )
-
-  } catch (error) {
-    return (
-      <div className="w-full">
-        OK
+      <div className="list-card-product py-3 grid grid-cols-5 gap-4">
+        {/* {
+          Array.from({ length: 5 }, (_, i) => i + 1).map(item => (
+            <CardProduct key={item} />
+          ))
+        } */}
+        {
+          <CardProduct data={arrayProducts} />
+        }
       </div>
-    )
-  }
-
-
+    </div>
+  )
 }
