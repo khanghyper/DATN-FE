357711d962b68ecaeab851908027368c38198eb4
Nguyễn Duy Khang <126125137+khanghyper@users.noreply.github.com>
2024-11-17 09:32:07 +0700

    Delete test.js

--- test.js
+++
@@ -1,4 +0,0 @@
-a = [
-  { name: 'color', values: [{ image: 'anh1-red.png', value: 'red' }, { image: 'anh2-blue.png', value: 'blue' }] },
-  { name: 'size', values: [{ image: '', value: 'L' }, { image: '', value: 'XL' }, { image: '', value: 'S' }] }
-]
